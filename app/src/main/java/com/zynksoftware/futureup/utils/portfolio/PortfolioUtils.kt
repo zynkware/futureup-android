4f4845e28b3a66be0518ab8d91283bc047ff2ef8
AdrianIosep <adrian.iosep@zynksoftware.com>
2022-03-22 13:39:03 +0200

    final

--- app/src/main/java/com/zynksoftware/futureup/utils/portfolio/PortfolioUtils.kt
+++ app/src/main/java/com/zynksoftware/futureup/utils/portfolio/PortfolioUtils.kt
@@ -7,12 +7,11 @@ object PortfolioUtils {
     fun getTotalBalance(): Double {
         var balance = 0.0
         for (coin in TemporaryDB.myCoins) {
-            if(coin.usdValue!=null){
+            if (coin.usdValue != null) {
 
                 balance += coin.usdValue
-            }
-            else{
-                balance+=0
+            } else {
+                balance += 0
             }
         }
         return balance
